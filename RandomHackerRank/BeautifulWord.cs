bb669ffae222c08b236033a4f2e8223517cddda4
masaab.algailani <masaab.algailani@integrapay.com.au>
2017-04-11 10:40:57 +1000

    Added Beautiful Word class

---
+++ RandomHackerRank/BeautifulWord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomHackerRank
+{
+    public class BeautifulWord
+    {
+        string[] vowels = { "a", "e", "i", "0", "u", "y" };
+        public bool CheckCorrectString(string data)
+        {
+            if (data.Length == 1)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                if (data[i] == data[i + 1])
+                {
+                    return false;
+                }
+                if (vowels.Contains(data[i].ToString()) && vowels.Contains(data[i + 1].ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- RandomHackerRankTest/RandomHackerRankTest.cs
+++ RandomHackerRankTest/RandomHackerRankTest.cs
@@ -1,42 +1,62 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RandomHackerRank;
+using Xunit;
 
 namespace RandomHackerRankTest
 {
-    [TestClass]
     public class RandomHackerRankTest
     {
-        [TestMethod]
+        [Fact]
         public void _2DArrayTest()
         {
             _2DArray array = new _2DArray();
             int? sum = array.GetLargestGlassHourSum();
-            Assert.AreEqual(-6, sum);
+            Assert.Equal(-6, sum);
         }
 
-        [TestMethod]
+        [Fact]
         public void MinimumFunctionTest()
         {
             MinimumFunction func = new MinimumFunction();
             string data = func.TotalMinimumFunctionCall();
-            Assert.AreEqual("min(int, min(int, min(int, int)))",data);
+            Assert.Equal("min(int, min(int, min(int, int)))",data);
         }
 
 
-        [TestMethod]
+        [Fact]
         public void MelodiousPassowrdTest()
         {
             MelodiousPassword test = new MelodiousPassword();
-           
         }
 
-        [TestMethod]
+        [Fact]
         public void ReplenishBowlTest()
         {
             ReplenishBowl test = new ReplenishBowl();
             var output = test.GetTotalExtraCandiesAdded(8,new int[] { 3, 1, 7, 5 }, 4);
-            Assert.AreEqual(11, output);
+            Assert.Equal(11, output);
+        }
+
+        [Theory]
+        [InlineData("apple", false)]
+        [InlineData("beauty", false)]
+        [InlineData("batman", true)]
+        [InlineData("superman", true)]
+        [InlineData("abyy", false)]
+        [InlineData("azei", false)]
+        [InlineData("ei", false)]
+        [InlineData("i", true)]
+        public void BeautifulWordTest(string data, bool expectedValue)
+        {
+            BeautifulWord t = new BeautifulWord();
+            var value = t.CheckCorrectString(data);
+            Assert.Equal(expectedValue, value);
+
+            //LinkListDemo demo = new LinkListDemo();
+            //demo.AddNode(new Node { Data = "No1"  });
+            //demo.AddNode(new Node { Data = "No2" });
+            //demo.AddNode(new Node { Data = "No3" });
+
         }
     }
 }
